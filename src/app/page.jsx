c9a186bf1784e79ad6c32e675e8d19e64601fb27
purya <porya.hajian79@gmail.com>
2024-11-03 00:25:54 +0330

    fixed home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,7 @@ import FeatureHomeTemplate from "./components/template/feature-home-template";
 import SliderMain from "./components/organisms/Slider";
 import Footer from "./components/template/footer";
 import Header from "./components/template/Header";
+import TabCategoryHome from "./components/template/tab-category-home";
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
       <FeatureHomeTemplate/>
       <div className="!mt-20 max-w-[1160px] m-auto px-3">
         <Title className={`max-[940px]:hidden`}>دسته بندی دوره ها</Title>
-        <FeatureHomeTemplate/>
+        <TabCategoryHome/>
       </div>
       <Footer/>
     </main>
